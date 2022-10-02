3275c8637fed93bea1dd3431a6e0fc13f2df1346
Asaad AbuGhazaleh <asaad.ag.98@gmail.com>
2022-10-02 18:41:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Asaad',
+    streetAdress: "Nablus, Ps",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    const newthing = {
+        ...employee,
+        [key]: value,
+    };
+    return newthing;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+
+    const newthing = { ...employee };
+
+    delete newthing[key];
+
+    return newthing
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+};
